d826d0e604438c54c9fafdff56eb66733ae34a37
Ben Eren <ben@Bens-MacBook-Air.local>
2025-02-04 22:26:42 +0000

    add login and register js update for user login and registration

--- client/login.js
+++ client/login.js
@@ -7,3 +7,30 @@ function buttonClick(e) {
     window.location.href = "http://127.0.0.1:5501/dashboard.html";
 }
 
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: form.get("studentLogin"),
+            password: form.get("password")
+        })
+    }
+
+    const response = await fetch("http://localhost:3000/users/login", options);
+    const data = await response.json();
+
+    if (response.status == 200) {
+        localStorage.setItem("token", data.token);
+        window.location.assign("board.html");
+      } else {
+        alert(data.error);
+      }
+})
--- client/register.js
+++ client/register.js
@@ -9,43 +9,31 @@ function buttonClick(e) {
     window.location.href = "http://127.0.0.1:5501/login.html";
 }
 
-const formSelection = document.querySelector('#registerForm')
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-formSelection.addEventListener('submit', getData)
+    const form = new FormData(e.target);
 
-function getData(e){
-    e.preventDefault()
-    const formData = new FormData(formSelection);
-    const formObject = Object.fromEntries(formData.entries()); // Convert FormData to an object
-    console.log(formObject)
-    // postCard(formObject)
-    formSelection.reset()
-    // comment out with backend fucntionality!!!
-    window.location.href = "http://127.0.0.1:5501/login.html";
-}    
-
-async function postCard(data) {
-    try {
-        console.log(data.date)
-        const response = await fetch(`http://localhost:3003/diary/`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                firstName: formObject.firstName,
-                lastName: formObject.lastName,
-                studentLogin: formObject.studentLogin,
-                password: formObject.password
-            })
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            firstName: form.get("firstName"),
+            lastName: form.get("lastName"),
+            studentLogin: form.get("studentLogin"),
+            password: form.get("password")
         })
-        if (response.ok) {xw
-            const postedCard = await response.json()
-            window.location.href = "http://127.0.0.1:5501/FrontEnd/login.html";
-        } else {
-            throw "Error http status code " + response.status
-        }
-    } catch (err) {
-        console.log(err)
     }
-}
+
+    const response = await fetch("http://localhost:3000/users/register", options);
+    const data = await response.json();
+
+    if (response.status == 201) {
+        window.location.assign("login.html");
+    } else {
+        alert(data.error);
+    }
+})
