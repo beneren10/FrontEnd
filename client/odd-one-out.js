3c354423e7f10f4ee933344f7c9be3e94358cefe
Ben Eren <ben@Bens-MacBook-Air.local>
2025-02-04 14:18:12 +0000

    progress on javascript game

--- client/odd-one-out.js
+++ client/odd-one-out.js
@@ -6,3 +6,32 @@ function buttonClick(e) {
     e.preventDefault()
     window.location.href = "http://www.google.co.uk";
 }
+
+const one = document.querySelector('#one p')
+const two = document.querySelector('#two p')
+const three = document.querySelector('#three p')
+const four = document.querySelector('#four p')
+
+async function fetchQuestions(){
+    const url = `localhost:3000/questions`
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok){
+            throw new Error(("This message is from space, run away now"))
+        }
+        const questions = await response.json()
+        add(questions)
+
+    }catch(error) {
+    console.log(error + "hello")
+    }
+}
+
+function add(questions){
+    const { a,b,c,d } = questions
+    addOne(a)
+    addTwo(b)
+    addThree(c)
+    addFour(d)
+}
